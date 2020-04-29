803e0b8f2ae3e0b7434d2b9fbe34276437150eff
fogfire <yangjusheng1314@gmail.com>
2020-04-29 23:53:39 +0800

    feature: 启动页

---
+++ lib/pages/home.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({ Key key, @required this.arg }) : super(key: key);
+
+  final String arg;
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text(widget.arg),
+      ),
+    );
+  }
+}
